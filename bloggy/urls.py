d6d592309c2d942e1741e8f1becf292935cbd486
Adham Magdy <adhammagdy.dev@gmail.com>
2022-07-26 23:15:17 +0200

    added favicon

--- bloggy/urls.py
+++ bloggy/urls.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from bloggy import settings, views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about, name='about'),
     path('', views.home, name='home'),
     path('captcha/', include('captcha.urls')),
-
+    re_path('favicon\.ico$', favicon_view),
     path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
 ]
---
+++ static/load.js
@@ -0,0 +1,7 @@
+
+const changeWidth = ()=>{
+    const widget = document.getElementById('cke_id_body');
+    widget.style.width = '100%';
+}
+
+setTimeout(changeWidth, 1000);
